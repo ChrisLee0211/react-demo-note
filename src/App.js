51da4e196e085d1aff93367e37b9b3c774053d7a
ChrisLee0211 <1018596741@qq.com>
2019-07-08 23:24:29 +0800

    新建项目，新增styled-componet

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CssStyle from './style'
+
+
+function App() {
+  return (
+      <div>
+      hello world
+      <CssStyle/>
+    </div>
+  );
+}
+
+export default App;
