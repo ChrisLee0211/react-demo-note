d5851f7cdd19e047fd7775d372ef886e0c7dde36
ChrisLee0211 <1018596741@qq.com>
2019-07-16 23:27:51 +0800

    新增immutbale做数据持久化

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -50,7 +50,7 @@ class Header extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        focused: state.header.focused
+        focused: state.get('header').get('focused')
     }
 }
 
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,19 +1,16 @@
 import * as constants from './constants';
+import {fromJS} from 'immutable';
 
-const defaultState = {
+const defaultState = fromJS({
     focused:false
-};
+});
 
 export default (state = defaultState,actions) =>{
     if(actions.type === constants.SEARCH_FOCUS){
-        return {
-            focused:true
-        }
+        return state.set('focused',true)
     };
     if(actions.type === constants.SEARCH_BLUR){
-        return {
-            focused:false
-        }
+        return state.set('focused',false)
     }
     
     return state
