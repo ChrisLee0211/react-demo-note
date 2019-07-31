6f36fccdc6547676416ef9a1a58524c61ed95877
Chrislee0211 <1018596741@qq.com>
2019-07-31 23:41:13 +0800

    新增react-router-dom作为路由

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import CssStyle from './style'
-import Css from './static/iconfont/iconfont'
+import CssStyle from './style';
+import Css from './static/iconfont/iconfont';
+import { BrowserRouter,Route, Router } from 'react-router-dom';
 import Header from './common/header';
 import store from './store';
 import {Provider} from 'react-redux';
+import Home from "./pages/home";
+import Detail from "./pages/detail"
 
-function App() {
-  return (
+class App extends React.Component {
+
+  render(){
+    return (
       <Provider store={store}>
-        <CssStyle/>
-        <Css/>
-        <Header></Header>
+        <div>
+          <CssStyle/>
+          <Css/>
+          <Header></Header>
+          <BrowserRouter>
+          <div>
+            <Route path='/' exact component={Home}></Route>
+            <Route path='/detail' exact component={Detail}></Route>
+          </div>
+          </BrowserRouter>
+        </div>
       </Provider>
-  );
-}
 
+    )
+  }
+}
 export default App;
