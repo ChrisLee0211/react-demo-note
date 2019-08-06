78c387a27ac150e0f35dc9bfa3c1ce4af837b32c
Chrislee0211 <1018596741@qq.com>
2019-08-06 23:06:01 +0800

    新增首页列表

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -12,7 +12,33 @@ const defaultState = fromJS({
             title:'手绘',
             imgUrl:'//upload-images.jianshu.io/upload_images/12713337-da89f65aec445915.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240"/'
         }
-    ]
+    ],
+    articleList:[
+        {
+            id:1,
+            title:'教育部：2018年这15个专业就业率最低！看起来都是好专业……',
+            desc: '新晚报 1月15日 很多高考学子们在高考路上拼尽全力过后，就到了一个重视程度丝毫不比高考轻的另外一程，那就是高考志愿的填报。 很多的高中学校也会...',
+            imgUrl:'//upload-images.jianshu.io/upload_images/12527628-a5816132e3a26e25.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+        },
+        {
+            id:2,
+            title:'教育部：2018年这15个专业就业率最低！看起来都是好专业……',
+            desc: '新晚报 1月15日 很多高考学子们在高考路上拼尽全力过后，就到了一个重视程度丝毫不比高考轻的另外一程，那就是高考志愿的填报。 很多的高中学校也会...',
+            imgUrl:'//upload-images.jianshu.io/upload_images/12527628-a5816132e3a26e25.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+        },
+        {
+            id:3,
+            title:'教育部：2018年这15个专业就业率最低！看起来都是好专业……',
+            desc: '新晚报 1月15日 很多高考学子们在高考路上拼尽全力过后，就到了一个重视程度丝毫不比高考轻的另外一程，那就是高考志愿的填报。 很多的高中学校也会...',
+            imgUrl:'//upload-images.jianshu.io/upload_images/12527628-a5816132e3a26e25.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+        },
+        {
+            id:4,
+            title:'教育部：2018年这15个专业就业率最低！看起来都是好专业……',
+            desc: '新晚报 1月15日 很多高考学子们在高考路上拼尽全力过后，就到了一个重视程度丝毫不比高考轻的另外一程，那就是高考志愿的填报。 很多的高中学校也会...',
+            imgUrl:'//upload-images.jianshu.io/upload_images/12527628-a5816132e3a26e25.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+        },
+]
 });
 
 export default (state = defaultState,actions) =>{
